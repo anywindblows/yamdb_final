fea001783205762fd023642a539477ce8fc9c44d
Alexander Kondratev <kondratev.av@inbox.ru>
2022-11-04 19:45:25 +0500

    fix req, workflow

--- api_yamdb/api_yamdb/wsgi.py
+++ api_yamdb/api_yamdb/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'api_yamdb.development'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'api_yamdb.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_yamdb.settings')
 
 application = get_wsgi_application()
